98793e66332a5af5a6a6773cb03562e81829b93a
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-06-10 02:29:34 +0900

    back to init state

--- src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
+++ src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
@@ -1,24 +1,15 @@
 package com.example.springsecuritystudy.study;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -26,24 +17,9 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-        .formLogin(form -> form
-//            .loginPage("/loginPage")
-            .loginProcessingUrl("/loginProc")
-            .defaultSuccessUrl("/", false)
-            .failureUrl("/failed")
-            .usernameParameter("userId")
-            .passwordParameter("passwd")
-            .successHandler((request, response, authentication) -> {
-              System.out.println("authentication: " + authentication);
-              response.sendRedirect("/home");
-            })
-            .failureHandler((request, response, exception) -> {
-              System.out.println("exception: " + exception.getMessage());
-              response.sendRedirect("/faileddd");
-            })
-            .permitAll()
-        );
+    http
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .formLogin(Customizer.withDefaults());
 
     return http.build();
   }
