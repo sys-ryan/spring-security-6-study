a62870785059c9d916c4f0e17efdfa0f1f0910e5
ryankim <sys.ryan0902@gmail.com>
2024-06-10 02:21:58 +0900

    section2-UsernamePasswordAuthenticationFilter

---
+++ src/main/java/com/example/springsecuritystudy/study/IndexController.java
@@ -0,0 +1,28 @@
+package com.example.springsecuritystudy.study;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping("/loginPage")
+  public String loginPage() {
+    return "loginPage";
+  }
+
+  @GetMapping("/home")
+  public String home() {
+    return "home";
+  }
+
+  @GetMapping("/faileddd")
+  public String failed() {
+    return "failed";
+  }
+}
---
+++ src/test/java/com/example/springsecuritystudy/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritystudy.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
