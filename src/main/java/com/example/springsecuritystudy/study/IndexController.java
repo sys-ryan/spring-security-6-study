c71083b28a71a533885aba344982d1c6defd93bc
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-06-17 18:12:51 +0900

    security context / security context holder

--- src/main/java/com/example/springsecuritystudy/study/IndexController.java
+++ src/main/java/com/example/springsecuritystudy/study/IndexController.java
@@ -1,5 +1,8 @@
 package com.example.springsecuritystudy.study;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,10 @@ public class  IndexController {
 
   @GetMapping("/")
   public String index() {
-    return "index";
+    SecurityContext securityContext = SecurityContextHolder.getContext();
+    Authentication authentication = securityContext.getAuthentication();
+    System.out.println("authentication = " + authentication);
+    return "";
   }
 
   @GetMapping("/loginPage")
