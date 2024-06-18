99c4e8f51e46abaf1d254609cba0bcd0c5c84daf
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-06-19 02:50:06 +0900

    authentication provider

---
+++ src/main/java/com/example/springsecuritystudy/study/CustomAuthenticationProvider.java
@@ -0,0 +1,29 @@
+package com.example.springsecuritystudy.study;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String loginId = authentication.getName();
+        String password = (String) authentication.getCredentials();
+
+        // validate id
+        // validate pw
+        // ...
+        System.out.println("custom authentication provider 1");
+
+        return new UsernamePasswordAuthenticationToken(loginId, password, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
+++ src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springsecuritystudy.study;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,9 +18,17 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    builder.authenticationProvider(new CustomAuthenticationProvider());
+    builder.authenticationProvider(new CustomAuthenticationProvider2());
+
     http
             .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .formLogin(Customizer.withDefaults());
+            .formLogin(Customizer.withDefaults())
+//            .authenticationProvider(new CustomAuthenticationProvider())
+//            .authenticationProvider(new CustomAuthenticationProvider2());
+    ;
 
     return http.build();
   }
