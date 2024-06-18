f82886d2f376827d7b61dcf3b135845c72f018e4
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-06-19 02:18:27 +0900

    authentication manager

--- src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
+++ src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
@@ -1,35 +1,50 @@
-package com.example.springsecuritystudy.study;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-@Configuration
-public class SecurityConfig {
-
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .formLogin(Customizer.withDefaults());
-
-    return http.build();
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user = User.withUsername("user")
-        .password("{noop}1111")
-        .roles("USER").build();
-
-    return new InMemoryUserDetailsManager(user);
-  }
-}
+//package com.example.springsecuritystudy.study;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfig {
+//
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+//    AuthenticationManager authenticationManager = builder.build();
+//
+//
+//    http
+//            .authorizeHttpRequests(auth -> auth
+//                    .requestMatchers("/", "/api/login").permitAll()
+//                    .anyRequest().authenticated())
+//            .authenticationManager(authenticationManager)
+//            .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
+//
+//    return http.build();
+//  }
+//
+//  public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+//    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+//    customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+//    return customAuthenticationFilter;
+//  }
+//
+//  @Bean
+//  public UserDetailsService userDetailsService() {
+//    UserDetails user = User.withUsername("user")
+//        .password("{noop}1111")
+//        .roles("USER").build();
+//
+//    return new InMemoryUserDetailsManager(user);
+//  }
+//}
