d27bce3fee12ce7e6505d3a9b4d9fb01d2c40397
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-06-20 19:30:42 +0900

    UserDetails

--- src/main/java/com/example/springsecuritystudy/study/CustomUserDetailsService.java
+++ src/main/java/com/example/springsecuritystudy/study/CustomUserDetailsService.java
@@ -1,15 +1,16 @@
 package com.example.springsecuritystudy.study;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
+
 public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User.withUsername("user")
-                .password("{noop}1111")
-                .roles("USER").build();
+        AccountDto accountDto = new AccountDto("user", "{noop}1111", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        return new CustomUserDetails(accountDto);
     }
 }
