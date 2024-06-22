6223155c9026e63ebf159e84981c7b0010ce16ac
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-06-22 21:59:06 +0900

    SecurityContextRepository / SecurityContextHolderFilter

--- src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
+++ src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.example.springsecuritystudy.study;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -17,11 +20,27 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    AuthenticationManager authenticationManager = builder.build();
+
     http
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .formLogin(Customizer.withDefaults());
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/login").permitAll()
+                    .anyRequest().authenticated())
+            .formLogin(Customizer.withDefaults())
+//            .securityContext(securityContext -> securityContext.requireExplicitSave(true))
+            .authenticationManager(authenticationManager)
+            .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
+  }
+
+  public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+    customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+    return customAuthenticationFilter;
+
   }
 
   @Bean
