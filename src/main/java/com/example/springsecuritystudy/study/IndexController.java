cb53c3baabf21af4d1fc7cc6ac25e92cbdd19807
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-07-06 13:25:09 +0900

    CSRF Token 유지 및 검증

--- src/main/java/com/example/springsecuritystudy/study/IndexController.java
+++ src/main/java/com/example/springsecuritystudy/study/IndexController.java
@@ -1,28 +1,54 @@
 package com.example.springsecuritystudy.study;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class  IndexController {
+public class IndexController {
 
-  @GetMapping("/")
-  public String index() {
-    return "index";
-  }
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-  @GetMapping("/loginPage")
-  public String loginPage() {
-    return "loginPage";
-  }
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
-  @GetMapping("/home")
-  public String home() {
-    return "home";
-  }
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
 
-  @GetMapping("/faileddd")
-  public String failed() {
-    return "failed";
-  }
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/faileddd")
+    public String failed() {
+        return "failed";
+    }
+
+    @PostMapping("/csrf")
+    public String csrf() {
+        return "csrf applied";
+    }
+
+    @GetMapping("/csrfToken")
+    public String csrfToken(HttpServletRequest request) {
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+//      CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+
+        logger.info("csrfToken: {}", csrfToken);
+        logger.info("csrfToken.getToken(): {}", csrfToken.getToken());
+        logger.info("csrfToken.getHeaderName(): {}", csrfToken.getHeaderName());
+        logger.info("csrfToken.getParameterName(): {}", csrfToken.getParameterName());
+
+        String token = csrfToken.getToken();
+        return token;
+    }
 }
--- src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
+++ src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
@@ -10,26 +10,42 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .formLogin(Customizer.withDefaults());
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    return http.build();
-  }
+        CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
+//    csrfTokenRepository.setCookieHttpOnly(false);
+//    csrfTokenRepository.setCookieName("CSRF_RYAN");
 
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user = User.withUsername("user")
-        .password("{noop}1111")
-        .roles("USER").build();
 
-    return new InMemoryUserDetailsManager(user);
-  }
+        XorCsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new XorCsrfTokenRequestAttributeHandler();
+//        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName(null);  //  지연된 방식이 아닌 바로 토큰을 얻도록 하는 옵션
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/csrf").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository)
+                        .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
