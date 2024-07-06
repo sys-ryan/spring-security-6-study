7d0e82b397785a204aee1d6378a035c5ea4fe841
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-07-06 20:42:02 +0900

    CSRF 통합

--- src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
+++ src/main/java/com/example/springsecuritystudy/study/SecurityConfig.java
@@ -10,8 +10,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -20,26 +20,48 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
-//    csrfTokenRepository.setCookieHttpOnly(false);
-//    csrfTokenRepository.setCookieName("CSRF_RYAN");
+        SpaCsrfTokenRequestHandler csrfTokenRequestHandler = new SpaCsrfTokenRequestHandler();
 
-
-        XorCsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new XorCsrfTokenRequestAttributeHandler();
-//        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName(null);  //  지연된 방식이 아닌 바로 토큰을 얻도록 하는 옵션
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/csrf").permitAll()
+                        .requestMatchers("/csrf", "/cookie", "/cookieCsrf").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository)
-                        .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
-                );
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(csrfTokenRequestHandler)
+                )
+                .addFilterBefore(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
 
         return http.build();
     }
 
+
+    /*
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    //        CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
+    //    csrfTokenRepository.setCookieHttpOnly(false);
+    //    csrfTokenRepository.setCookieName("CSRF_RYAN");
+
+
+            XorCsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new XorCsrfTokenRequestAttributeHandler();
+    //        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName(null);  //  지연된 방식이 아닌 바로 토큰을 얻도록 하는 옵션
+
+            http
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/csrf", "csrfToken", "/form", "/formCsrf").permitAll()
+                            .anyRequest().authenticated())
+                    .formLogin(Customizer.withDefaults())
+                    .csrf(Customizer.withDefaults());
+    //                .csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository)
+    //                        .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+    //                );
+
+            return http.build();
+        }
+    */
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user")
